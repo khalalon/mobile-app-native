07c2a567c70853cfa516471ff41daf678ed0e66e
khalalon <khalilkossentini69@gmail.com>
2025-02-26 10:59:31 +0100

    add fonts and color into tailwind

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-        <Text className="font-bold text-lg my-10">Welcome to Mobile Application</Text>
+        <Text className="font-bold text-3xl font-rubik my-10">Welcome to Mobile Application</Text>
         <Link href="/sign-in">Sign in </Link>
         <Link href="/explore">Explore </Link>
         <Link href="/profile">Profile </Link>
