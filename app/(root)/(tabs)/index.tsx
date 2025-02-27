8bf7d1973a207c26870d2158bf852d6732127e55
khalalon <khalilkossentini69@gmail.com>
2025-02-27 12:37:42 +0100

    fix some metro issues

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,22 +1,31 @@
 import { Text, View } from "react-native";
-import {Link} from "expo-router";
-
+import { Link } from "expo-router";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-        <Text className="font-bold text-3xl font-rubik my-10">Welcome to Mobile Application</Text>
-        <Link href="/sign-in">Sign in </Link>
-        <Link href="/explore">Explore </Link>
-        <Link href="/profile">Profile </Link>
-        <Link href="/properties/1">Property </Link>
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text style={{ fontSize: 24, fontWeight: "bold", marginVertical: 10 }}>
+                Welcome to Mobile Application
+            </Text>
 
-    </View>
-  );
+            <Link href="/sign-in">
+                <Text style={{ color: "blue", fontSize: 18 }}>Sign in</Text>
+            </Link>
+            <Link href="/explore">
+                <Text style={{ color: "blue", fontSize: 18 }}>Explore</Text>
+            </Link>
+            <Link href="/profile">
+                <Text style={{ color: "blue", fontSize: 18 }}>Profile</Text>
+            </Link>
+            <Link href="/properties/1">
+                <Text style={{ color: "blue", fontSize: 18 }}>Property</Text>
+            </Link>
+        </View>
+    );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,12 +6,12 @@ import {useEffect} from "react";
 export default function RootLayout() {
   const [fontsLoaded] = useFonts(
       {
-        "Rubik-Bold": require("./assets/fonts/Rubik-Bold.ttf"),
-        "Rubik-ExtraBold": require("./assets/fonts/Rubik-ExtraBold.ttf"),
-        "Rubik-Medium": require("./assets/fonts/Rubik-Medium.ttf"),
-        "Rubik-SemiBold": require("./assets/fonts/Rubik-SemiBold.ttf"),
-        "Rubik-Regular": require("./assets/fonts/Rubik-Regular.ttf"),
-        "Rubik-Light": require("./assets/fonts/Rubik-Light.ttf")
+        "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+        "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+        "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+        "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+        "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+        "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf")
 
       }
   );
